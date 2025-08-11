86b98c7acd29a2146a47c9d9784d3209bada2cdd
Alex <alex@gogl.io>
2025-08-11 14:40:18 +0200

    initial 3

--- working/package-a/src/react/index.ts
+++ working/package-a/src/react/index.ts
@@ -1,1 +1,2 @@
 export type { Toast } from "../types"
+export const testValue = 1;
---
+++ working/package-b/src/react/index.ts
@@ -0,0 +1,4 @@
+export type { Toast } from "../types";
+import { testValue } from "@repro/package-a/react";
+
+export const otherTestValue = testValue;
